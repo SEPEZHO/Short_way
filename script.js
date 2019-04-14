256c4c125f5c3e343c5550a413778ee501b90cff
SEPEZHO <47150504+SEPEZHO@users.noreply.github.com>
2019-04-14 19:42:35 +0300

    ФЫВ
    Ф

--- script.js
+++ script.js
@@ -1,66 +1,180 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+var canvas = document.getElementById('c1');
+var ctx = canvas.getContext('2d');
 var mas = [];
-var width = 500;
-var height = 500;
+var mas2 = [];
+var count = 0;
+var timer;
+var width = 1280;
+var height = 720;
+var fps = document.getElementById('range');
 
-$(canvas).click(function (e) {
+canvas.onclick = function (event) {
   var x = event.offsetX;
   var y = event.offsetY;
-  x = Math.floor(x / 25);
-  y = Math.floor(y / 25);
-  if (e.ctrlKey) {
-    mas[x,y] = 2;
-  } else if (e.shiftKey) {
-    mas[x,y] = 3;
-  } else {
-    mas[x,y] = 1;
+  var i = x;
+  var j = y;
+  console.log(x);
+  console.log(y);
+  x = Math.floor(x / 10);
+  y = Math.floor(y / 10);
+  if (mas[y][x] == 0) {
+    mas[y][x] = 1;
+  } else if (mas[y][x] == 1) {
+    mas[y][x] = 2;
   }
-  fillRect(x, y);
-});
-
-function fillRect(x, y) {
-  if (mas[x,y] == 1) {
-    ctx.fillStyle = 'black';
-    ctx.fillRect(x * 25, y * 25, 25, 25);
-  }
-  if (mas[x,y] == 2) {
-    ctx.fillStyle = 'lime';
-    ctx.fillRect(x * 25, y * 25, 25, 25);
-  }
-  if (mas[x,y] == 3) {
-    ctx.fillStyle = 'red';
-    ctx.fillRect(x * 25, y * 25, 25, 25);
-  }
+  //(mas[y][x] == 0) ? mas[y][x] = 1: ctx.clearRect(y * 10, x * 10, 10, 10);
+  console.log(mas);
+  //ctx.clearRect(0, 0, width, height);
+  drawField();
 }
 
-function goSearch() {
-  for (var i = 0; i < width; i++) {
-    for (var j = 0; j < height; j++) {
-      if(mas[i, j] == 1){
-        ctx.fillStyle = 'lime';
-        ctx.fillRect(i * 25, j * 25, 20, 20);
-      }
+// mas.mouseover = function () {
+//   ctx.fillRect(j * 10, i * 10, 9.9, 9.9);
+//
+// }
+
+
+function goLife() {
+  var n = width,
+    m = height;
+  for (var i = 0; i < n; i++) {
+    mas[i] = [];
+    for (var j = 0; j < m; j++) {
+      mas[i][j] = 0;
     }
   }
 }
+goLife();
 
-function begin() {
-  console.log('Hello World!');
+function stroke() {
+  for (var x = 0.5; x <= width; x += 10) {
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, width);
+  }
+  for (var y = 0.5; y <= width; y += 10) {
+    ctx.moveTo(0, y);
+    ctx.lineTo(width, y);
+  }
+  ctx.strokeStyle = 'black';
+  ctx.stroke();
 }
 
-$('body').keypress(function (e) {
-  if (e.which == 13) {
-    goSearch();
-    console.log('Hello World!');
-  }
-});
-
-// function getelem() {
-//   for (var i = 0; i < width; i++) {
-//     for (var j = 0; j < height; j++) {
-//        ctx.fillStyle = 'black';
-//        ctx.fillRect(i * 25, j * 25, 25, 25);
-//     }
-//   }
+// document.getElementById('randColor').onclick = function ColorR(){
+//   var r = Math.floor(Math.random() * (256)),
+//       g = Math.floor(Math.random() * (256)),
+//       b = Math.floor(Math.random() * (256));
+//   ctx.fillStyle = '#' + r.toString(16) + g.toString(16) + b.toString(16);
+//   ctx.fillRect(j * 10, i * 10, 10, 10);
 // }
+
+function drawField() {
+  //ctx.clearRect(0, 0, width, height);
+  for (var i = 0; i < width; i++) {
+    for (var j = 0; j < height; j++) {
+      if (mas[i][j] == 1) {
+        ctx.fillStyle = 'lime';
+        ctx.fillRect(j * 10, i * 10, 9.9, 9.9);
+      } else if (mas[i][j] == 2) {
+        ctx.fillStyle = 'hsl(0, 0%, 15%)';
+        ctx.fillRect(j * 10, i * 10, 9.9, 9.9);
+        mas[i][j] = 0;
+      }
+    }
+  }
+  stroke();
+}
+stroke();
+
+function startLife() {
+  var mas2 = [];
+  for (var i = 0; i < width; i++) {
+    mas2[i] = [];
+    for (var j = 0; j < height; j++) {
+      var neighbors = 0;
+      if (mas[fpm(i) - 1][j] == 1) neighbors++; //up
+      if (mas[i][fpp(j) + 1] == 1) neighbors++; //right
+      if (mas[fpp(i) + 1][j] == 1) neighbors++; //bottom
+      if (mas[i][fpm(j) - 1] == 1) neighbors++; //left
+      if (mas[fpm(i) - 1][fpp(j) + 1] == 1) neighbors++;
+      if (mas[fpp(i) + 1][fpp(j) + 1] == 1) neighbors++;
+      if (mas[fpp(i) + 1][fpm(j) - 1] == 1) neighbors++;
+      if (mas[fpm(i) - 1][fpm(j) - 1] == 1) neighbors++;
+      if (mas[i][j] == 0 && neighbors == 3) {
+        mas2[i][j] = 1;
+      } else {
+        if (neighbors == 2 && mas[i][j] == 1 || neighbors == 3 && mas[i][j] == 1) {
+          mas2[i][j] = 1;
+        } else {
+          mas2[i][j] = 0;
+        }
+      }
+      if (mas[i][j] == 1 && mas2[i][j] == 0) {
+        ctx.fillStyle = 'hsl(0, 0%, 15%)';
+        ctx.fillRect(j * 10, i * 10, 9.9, 9.9);
+      }
+    }
+  }
+  mas = mas2;
+  drawField();
+  reset()
+}
+
+function reset() {
+  count++;
+  document.getElementById('count').innerHTML = count;
+  var speed = range.value;
+  var fps = document.getElementById('range');
+  // console.log(fps);
+  timer = setTimeout(startLife, 1000 / 21); // тут вместо 25 должна быть переменная speed, но я ее не использую, тк в ней вся проблема.
+  var p = document.getElementById('p');
+  p.innerHTML = fps.value - 1;
+}
+
+function stopLife() {
+  clearTimeout(timer);
+}
+
+function resetLife() {
+  ctx.clearRect(0, 0, width, height);
+  var n = width;
+  var m = height;
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < m; j++) {
+      mas[i][j] = 0;
+    }
+  }
+  count = 0;
+  document.getElementById('count').innerHTML = count;
+  stopLife();
+  console.log('Reset');
+}
+
+function randLife() {
+  resetLife();
+
+  for (var i = 0; i < width; i++) {
+    for (var j = 0; j < height; j++) {
+      var randInt = Math.floor(Math.random() * 2) + 0;
+      mas[i][j] = randInt;
+      if (mas[i][j] == 1) {
+        ctx.fillStyle = 'lime';
+        ctx.fillRect(j * 10, i * 10, 9.9, 9.9);
+      }
+    }
+  }
+  stroke();
+}
+
+function fpm(i) {
+  if (i == 0) return width;
+  else return i;
+}
+
+function fpp(i) {
+  if (i == width - 1) return -1;
+  else return i;
+}
+document.getElementById('start').onclick = startLife;
+document.getElementById('stop').onclick = stopLife;
+document.getElementById('res').onclick = resetLife;
+document.getElementById('rand').onclick = randLife;
