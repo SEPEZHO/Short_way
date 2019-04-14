c0f3b6a06761d849bde54fa2cbe16556633aba40
SEPEZHO <47150504+SEPEZHO@users.noreply.github.com>
2019-04-14 21:41:40 +0300

    a

--- script.js
+++ script.js
@@ -7,24 +7,37 @@ var timer;
 var width = 1280;
 var height = 720;
 var fps = document.getElementById('range');
+var xxx2 = 0;
+var yyy2 = 0;
+var xxx3 = 0;
+var yyy3 = 0;
 
 canvas.onclick = function (event) {
   var x = event.offsetX;
   var y = event.offsetY;
-  var i = x;
-  var j = y;
   console.log(x);
   console.log(y);
   x = Math.floor(x / 10);
   y = Math.floor(y / 10);
-  if (mas[y][x] == 0) {
-    mas[y][x] = 1;
-  } else if (mas[y][x] == 1) {
+  xxx = x;
+  yyy = y;
+  if (event.ctrlKey) {
     mas[y][x] = 2;
+    xxx2 = x;
+    yyy2 = y;
+  } else if (event.shiftKey) {
+    mas[y][x] = 3;
+    xxx3 = x;
+    yyy3 = y;
+  } else {
+    mas[y][x] = 1;
+    xxx = x;
+    yyy = y;
   }
-  //(mas[y][x] == 0) ? mas[y][x] = 1: ctx.clearRect(y * 10, x * 10, 10, 10);
+  if (mas[y][x] != 0) {
+    mas[y][x] == 0;
+  }
   console.log(mas);
-  //ctx.clearRect(0, 0, width, height);
   drawField();
 }
 
@@ -77,7 +90,10 @@ function drawField() {
       } else if (mas[i][j] == 2) {
         ctx.fillStyle = 'hsl(0, 0%, 15%)';
         ctx.fillRect(j * 10, i * 10, 9.9, 9.9);
-        mas[i][j] = 0;
+      } else if (mas[i][j] == 3) {
+        ctx.fillStyle = 'rgb(255, 128, 0)';
+        ctx.fillRect(j * 10, i * 10, 9.9, 9.9);
+
       }
     }
   }
@@ -87,45 +103,76 @@ stroke();
 
 function startLife() {
   var mas2 = [];
+
   for (var i = 0; i < width; i++) {
     mas2[i] = [];
     for (var j = 0; j < height; j++) {
+      if (mas[i][j] == 2) {
+        var x2 = xxx2;
+        var y2 = yyy2;
+        console.log(x2, y2);
+      }
+      if (mas[i][j] == 3) {
+        var x3 = xxx3;
+        var y3 = yyy3;
+        console.log(x3, y3);
+      }
+      var a = x2 - x3;
+      var b = y2 - y3;
+      var c = Math.sqrt(a * a + b * b);
+
+      // if (mas[i - 1][j] == 0) {
+      //   var xx = xxx2 - 1;
+      //   var yy = yyy2;
+      //   var aa = xx - x3;
+      //   var bb = yy - y3;
+      //   var c1 = Math.sqrt(aa * aa + bb * bb);
+      // }
+      // if (mas[i + 1][j] == 0) {
+      //   var xx1 = xxx2 + 1;
+      //   var yy1 = yyy2;
+      //   var aa1 = xx1 - x3;
+      //   var bb1 = yy1 - y3;
+      //   var c2 = Math.sqrt(aa1 * aa1 + bb1 * bb1);
+      // }
+      // if (mas[i][j + 1] == 0) {
+      //   var xx2 = xxx2;
+      //   var yy2 = yyy2 + 1;
+      //   var aa2 = xx2 - x3;
+      //   var bb2 = yy2 - y3;
+      //   var c3 = Math.sqrt(aa2 * aa2 + bb2 * bb2);
+      // }
+      // if (mas[i][j - 1] == 0) {
+      //   var xx3 = xxx2;
+      //   var yy3 = yyy2 - 1;
+      //   var aa3 = xx3 - x3;
+      //   var bb3 = yy3 - y3;
+      //   var c4 = Math.sqrt(aa3 * aa3 + bb3 * bb3);
+      // }
+      // console.log(c, c1, c2, c3, c4);
+      // var n = 3;
+      // var m = Math.pow(10, n);
+      // c = Math.round(c * m) / m;
+      console.log(c);
+      console.log('sdasd');
       var neighbors = 0;
-      if (mas[fpm(i) - 1][j] == 1) neighbors++; //up
-      if (mas[i][fpp(j) + 1] == 1) neighbors++; //right
-      if (mas[fpp(i) + 1][j] == 1) neighbors++; //bottom
-      if (mas[i][fpm(j) - 1] == 1) neighbors++; //left
-      if (mas[fpm(i) - 1][fpp(j) + 1] == 1) neighbors++;
-      if (mas[fpp(i) + 1][fpp(j) + 1] == 1) neighbors++;
-      if (mas[fpp(i) + 1][fpm(j) - 1] == 1) neighbors++;
-      if (mas[fpm(i) - 1][fpm(j) - 1] == 1) neighbors++;
-      if (mas[i][j] == 0 && neighbors == 3) {
-        mas2[i][j] = 1;
-      } else {
-        if (neighbors == 2 && mas[i][j] == 1 || neighbors == 3 && mas[i][j] == 1) {
-          mas2[i][j] = 1;
-        } else {
-          mas2[i][j] = 0;
-        }
-      }
-      if (mas[i][j] == 1 && mas2[i][j] == 0) {
-        ctx.fillStyle = 'hsl(0, 0%, 15%)';
-        ctx.fillRect(j * 10, i * 10, 9.9, 9.9);
-      }
+      // if mas[i - 1][j]
+      // if mas[i + 1][j]
+      // if mas[i][j + 1]
+      // if mas[i][j - 1]
     }
   }
-  mas = mas2;
+  // mas = mas2;
   drawField();
-  reset()
+  // reset()
 }
-
 function reset() {
   count++;
   document.getElementById('count').innerHTML = count;
   var speed = range.value;
   var fps = document.getElementById('range');
   // console.log(fps);
-  timer = setTimeout(startLife, 1000 / 21); // тут вместо 25 должна быть переменная speed, но я ее не использую, тк в ней вся проблема.
+  // timer = setTimeout(startLife, 1000 / 21); // тут вместо 25 должна быть переменная speed, но я ее не использую, тк в ней вся проблема.
   var p = document.getElementById('p');
   p.innerHTML = fps.value - 1;
 }
