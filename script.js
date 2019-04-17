ebf2a9fee446e6ce1b6c5cdb58ea678bd90edd1f
SEPEZHO <47150504+SEPEZHO@users.noreply.github.com>
2019-04-17 22:03:50 +0300

    Ф

--- script.js
+++ script.js
@@ -34,9 +34,6 @@ canvas.onclick = function (event) {
     xxx = x;
     yyy = y;
   }
-  // if (mas[y][x] != 0) {
-  //   mas[y][x] == 0;
-  // }
   console.log(mas);
   drawField();
 }
@@ -71,7 +68,7 @@ function drawField() {
 }
 
 function startLife() {
-var   mas2=[];
+  var mas2 = [];
   for (var i = 0; i < width / 10; i++) {
     mas2[i] = [];
     for (var j = 0; j < height / 10; j++) {
@@ -88,28 +85,28 @@ var   mas2=[];
       var a = x2 - x3;
       var b = y2 - y3;
       var c = Math.sqrt(a * a + b * b);
-      if (mas[fpm(i) - 1][j] == 0) { // up
+      if (mas[fpm(i) - 1][j] == 0) { // rigth
         var xx = xxx2 - 1;
         var yy = yyy2;
         var aa = xx - x3;
         var bb = yy - y3;
         var c1 = Math.sqrt(aa * aa + bb * bb);
       }
-      if (mas[fpp(i) + 1][j] == 0) { //bottom
+      if (mas[fpp(i) + 1][j] == 0) { //left
         var xx1 = xxx2 + 1;
         var yy1 = yyy2;
         var aa1 = xx1 - x3;
         var bb1 = yy1 - y3;
         var c2 = Math.sqrt(aa1 * aa1 + bb1 * bb1);
       }
-      if (mas[i][fpp(j) + 1] == 0) { // right
+      if (mas[i][fpp(j) + 1] == 0) { // up
         var xx2 = xxx2;
         var yy2 = yyy2 + 1;
         var aa2 = xx2 - x3;
         var bb2 = yy2 - y3;
         var c3 = Math.sqrt(aa2 * aa2 + bb2 * bb2);
       }
-      if (mas[i][fpm(j) - 1] == 0) { // left
+      if (mas[i][fpm(j) - 1] == 0) { // bottom
         var xx3 = xxx2;
         var yy3 = yyy2 - 1;
         var aa3 = xx3 - x3;
@@ -117,39 +114,41 @@ var   mas2=[];
         var c4 = Math.sqrt(aa3 * aa3 + bb3 * bb3);
       }
       var min = Math.min(c, c1, c2, c3, c4);
-       // console.log(min);
       if (mas[i][j] == 3) {
         if (min == c1) {
+
           mas2[i][j + 1] = 3;
           console.log('c1');
-        }
-        if (min == c2) {
+        } else if (min == c2) {
+
           mas2[i][j - 1] = 3;
           console.log('c2');
-        }
-        if (min == c3) {
-          mas2[i - 1][j] = 3;
-          console.log('c3');
+        } else if (min == c3) {
 
-        }
-        if (min == c4) {
-          mas2[i][j - 1] = 3;
+          console.log('c3');
+          mas2[i - 1][j] = 3;
+        } else if (min == c4) {
+
           console.log('c4');
-        }
-        if (min == c) {
+          mas2[i+1][j] = 3;
+        } else if (min == c) {
+
           console.log('End.');
         }
-
-      }
-      if (mas[i][j] == 3) {
-        mas2[i][j] = 1;
       }
     }
   }
-  // console.log(min);
+  console.log(mas2 + '= mas2');
+  console.log(c1 + '= c1 (rigth)');
+  console.log(c2 + '= c2 (left)');
+  console.log(c3 + '= c3 (up)');
+  console.log(c4+ '= c4 (Bottom)');
+  console.log(min);
+  if (min == c3) console.log(' min is c3');
+  if (min == c4) console.log(' min is c4');
   mas = mas2;
   drawField();
-  timer = setTimeout(startLife, 100);
+  // timer = setTimeout(startLife, 100);
 }
 
 function fpm(i) {
