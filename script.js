f6241b2b019579ca3331d8dee6622db0ad913c0e
SEPEZHO <47150504+SEPEZHO@users.noreply.github.com>
2019-04-18 18:53:52 +0300

    A

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ var yyy2 = 0;
 var xxx3 = 0;
 var yyy3 = 0;
 
-canvas.onclick = function (event) {
+canvas.onclick = function ibolit(event) {
   var x = event.offsetX;
   var y = event.offsetY;
-  console.log(x);
-  console.log(y);
   x = Math.floor(x / 10);
   y = Math.floor(y / 10);
   xxx = x;
   yyy = y;
+  console.log(x);
+  console.log(y);
   if (event.ctrlKey) {
     mas[y][x] = 2;
     xxx2 = x;
@@ -38,6 +38,17 @@ canvas.onclick = function (event) {
   drawField();
 }
 
+// function coordinati(event){
+//   var x = ctx.GetLeft(canvas);
+//   var y = ctx.GetTop(canvas);
+//   console.log(x+ 'asa');
+//   console.log(y+'asasasa');
+//   x = Math.floor(x / 10);
+//   y = Math.floor(y / 10);
+//   xxx3 = x;
+//   yyy3 = y;
+// }
+// coordinati();
 function goLife() {
   var n = width,
     m = height;
@@ -69,7 +80,8 @@ function drawField() {
     }
   }
 }
-
+var ii;
+var jj;
 function startLife() {
   var mas2 = [];
   outer: for (var i = 0; i < width / 10; i++) {
@@ -81,35 +93,40 @@ function startLife() {
         console.log(x2, y2);
       }
       if (mas[i][j] == 3) {
+        // coordinati();
+
         var x3 = xxx3;
         var y3 = yyy3;
+        mas[i][j] = 1 ;
+        ii = i;
+        jj = j;
         console.log(x3, y3);
       }
       var a = x2 - x3;
       var b = y2 - y3;
       var c = Math.sqrt(a * a + b * b);
-      if (mas[fpm(i) - 1][j] == 0) { // rigth
+      if (mas[fpm(i) - 1][j] != 1) { // rigth
         var xx = xxx2 - 1;
         var yy = yyy2;
         var aa = xx - x3;
         var bb = yy - y3;
         var c1 = Math.sqrt(aa * aa + bb * bb);
       }
-      if (mas[fpp(i) + 1][j] == 0) { //left
+      if (mas[fpp(i) + 1][j] != 1) { //left
         var xx1 = xxx2 + 1;
         var yy1 = yyy2;
         var aa1 = xx1 - x3;
         var bb1 = yy1 - y3;
         var c2 = Math.sqrt(aa1 * aa1 + bb1 * bb1);
       }
-      if (mas[i][fpp(j) + 1] == 0) { // up
+      if (mas[i][fpp(j) + 1] != 1) { // up
         var xx2 = xxx2;
         var yy2 = yyy2 + 1;
         var aa2 = xx2 - x3;
         var bb2 = yy2 - y3;
         var c3 = Math.sqrt(aa2 * aa2 + bb2 * bb2);
       }
-      if (mas[i][fpm(j) - 1] == 0) { // bottom
+      if (mas[i][fpm(j) - 1] != 1) { // bottom
         var xx3 = xxx2;
         var yy3 = yyy2 - 1;
         var aa3 = xx3 - x3;
@@ -117,41 +134,39 @@ function startLife() {
         var c4 = Math.sqrt(aa3 * aa3 + bb3 * bb3);
       }
       var min = Math.min(c, c1, c2, c3, c4);
-      // if(mas[i][j] == 3) mas[i][j] = mas2[i][j];
-      if (mas[i][j] == 3) {
-        for (var i1 = i; i1 < width ; i1++) {
-            mas[i1] = 4;
-        }
-      }
-      if (min == c1) {
 
-        mas[i][j + 1] = 4;
-        // console.log(mas);
-        console.log(i, j);
-        console.log('c1');
-        break outer;
-      } else if (min == c2) {
+      // if (mas[i][j] == 3) { // ЭТО ДЛЯ ФИКСА БАГА С НИЗОМ. ЕСЛИ ОНО НАХОДИТ 3, ТО ВЫВЕДЕТ ЕГО ПОЗИЦИЮ.
+      //   ii = i;
+      //   jj = j;
+        // mas[i][j] = 1;
+    // }
 
-        mas[i][j - 1] = 4;
-        console.log('c2');
-        break outer;
-      } else if (min == c3) {
-
-        console.log('c3');
-        mas[i - 1][j] = 4;
-        break outer;
-      } else if (min == c4) {
-
-        console.log('c4');
-        mas[i + 1][j] = 4;
-        break outer;
-      } else if (min == c) {
-        console.log('End.');
-        break outer;
-
-      }
     }
   }
+
+  if (min == c1) {
+    mas[ii][jj + 1] = 3;
+    yyy3++;
+    console.log('c1');
+  } else if (min == c2) {
+
+    mas[ii][jj - 1] = 3;
+    console.log('c2');
+    yyy3--;
+  } else if (min == c3) {
+
+    mas[ii - 1][jj] = 3;
+    console.log('c3');
+    xxx3--;
+  } else if (min == c4) {
+
+    console.log('c4');
+    mas[ii + 1][jj] = 3;
+    xxx3++;
+  } else if (min == c) {
+    console.log('End.');
+  }
+
   // mas = mas2;
   console.log(mas + '= mas2');
   console.log(c1 + '= c1 (rigth)');
@@ -161,8 +176,10 @@ function startLife() {
   console.log(min);
   if (min == c3) console.log(' min is c3');
   if (min == c4) console.log(' min is c4');
+  // mas[ii][jj] = 1;
   drawField();
-  // timer = setTimeout(startLife, 100);
+
+  timer = setTimeout(startLife, 500);
 }
 
 function fpm(i) {
@@ -176,6 +193,14 @@ function fpp(i) {
 }
 document.getElementById('start').onclick = startLife;
 
+//  if(mas[i][j] == 3) mas[i][j] = mas2[i][j];
+// if (mas[i][j] == 3) {
+  //   for (var i1 = i; i1 < width; i1++) {
+    //     for (var j1 = 0; j1 < height; j1++) {
+      //       if (mas[i1][j1] == 2) mas[i1][j1] = 1;
+      //     }
+      //   }
+      // }
 // function reset() {
 //   count++;
 //   p.innerHTML = fps.value - 1;
