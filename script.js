cab02fc20ca1f9983c7eaa0cbbd35dfcf2f2fd52
SEPEZHO <47150504+SEPEZHO@users.noreply.github.com>
2019-04-19 20:47:03 +0300

    a

--- script.js
+++ script.js
@@ -85,78 +85,94 @@ function goSearch() {
       var a = x2 - x3;
       var b = y2 - y3;
       var c = Math.sqrt(a * a + b * b);
-      var min = Math.min(c, c1, c2, c3, c4);
 
-      if (mas[i - 1][j] != 0) { // rigth
-        var xx = xxx2 - 1;
-        var yy = yyy2;
-        var aa = xx - x3;
-        var bb = yy - y3;
-        c1 = Math.sqrt(aa * aa + bb * bb);
-        console.log(mas[i-1][j]+' =rigth');
-      } else {
-        c1 = 100;
-        console.log(mas[i-1][j]+' =rigth');
-      }
+      var xx = xxx2 - 1;
+      var yy = yyy2;
+      var aa = xx - x3;
+      var bb = yy - y3;
+      c1 = Math.sqrt(aa * aa + bb * bb);
+      // console.log(mas[i-1][j]+' =rigth');
 
-      if (mas[i + 1][j] != 0) { //left
-        var xx1 = xxx2 + 1;
-        var yy1 = yyy2;
-        var aa1 = xx1 - x3;
-        var bb1 = yy1 - y3;
-        c2 = Math.sqrt(aa1 * aa1 + bb1 * bb1);
-        console.log(mas[i+1][j]+' =left');
-      } else {
-        c2 = 100;
-        console.log(mas[i+1][j]+' =left');
-      }
+      var xx1 = xxx2 + 1;
+      var yy1 = yyy2;
+      var aa1 = xx1 - x3;
+      var bb1 = yy1 - y3;
+      c2 = Math.sqrt(aa1 * aa1 + bb1 * bb1);
+      // console.log(mas[i+1][j]+' =left');
 
-      if (mas[i][j + 1] != 0) { // up
-        var xx2 = xxx2;
-        var yy2 = yyy2 + 1;
-        var aa2 = xx2 - x3;
-        var bb2 = yy2 - y3;
-        c3 = Math.sqrt(aa2 * aa2 + bb2 * bb2);
-        console.log(mas[i][j+1]+' =up');
-      } else {
-        c3 = 100;
-        console.log(mas[i][j+1]+' =up');
-      }
+      var xx2 = xxx2;
+      var yy2 = yyy2 + 1;
+      var aa2 = xx2 - x3;
+      var bb2 = yy2 - y3;
+      c3 = Math.sqrt(aa2 * aa2 + bb2 * bb2);
+      // console.log(mas[i][j+1]+' =up');
 
-      if (mas[i][j - 1] != 0) { // bottom
-        var xx3 = xxx2;
-        var yy3 = yyy2 - 1;
-        var aa3 = xx3 - x3;
-        var bb3 = yy3 - y3;
-        c4 = Math.sqrt(aa3 * aa3 + bb3 * bb3);
-        console.log(mas[i][j-1]+' =bottom');
-      } else {
-        c4 = 100;
-        console.log(mas[i][j-1]+' =bottom');
+      var xx3 = xxx2;
+      var yy3 = yyy2 - 1;
+      var aa3 = xx3 - x3;
+      var bb3 = yy3 - y3;
+      c4 = Math.sqrt(aa3 * aa3 + bb3 * bb3);
+      // console.log(mas[i][j-1]+' =bottom');
 
-      }
     }
   }
+  if (mas[ii][jj+1] !=0) { // rigth
+    c1 = 100;
+    // console.log(mas[i-1][j]+' =rigth1');
+  }
 
-  if (min == c1) {
+  if (mas[ii][jj-1] !=0) { //left
+    c2 = 100;
+    // console.log(mas[i+1][j]+' =left1');
+  }
+  if (mas[ii-1][jj] !=0) { // up
+    c3 = 100;
+    // console.log(mas[i][j+1]+' =up1');
+  }
+  if (mas[ii+1][jj] !=0) { // bottom
+    c4 = 100;
+    // console.log(mas[i][j-1]+' =bottom1');
+  }
+  // var min;
+  // for(var minA = 0; minA <= 4;minA++){
+  //   if(('c'+minA) != undefined){
+  //     if(min<='c'+minA){
+  //       min = 'c'+minA;
+  //       console.log('c'+minA,min);
+  //     }
+  //   }
+  // }
+  var min = Math.min(c, c1, c2, c3, c4);
+   console.log(c1, c2, c3, c4);
+
+   console.log(min +'= min1');
+
+  if (min == c1 ) {
     mas[ii][jj + 1] = 3;
     xxx3++;
-  } else if (min == c2) {
+    console.log('c1');
+  } else if (min == c2  ) {
     mas[ii][jj - 1] = 3;
     xxx3--;
-  } else if (min == c3) {
+    console.log('c2');
+
+  } else if (min == c3 ) {
     mas[ii - 1][jj] = 3;
+
+    console.log('c3');
     yyy3--;
-  } else if (min == c4) {
+  } else if (min == c4 ) {
     mas[ii + 1][jj] = 3;
+    console.log('c4');
     yyy3++;
   }
 
   console.log(c1 + '= c1 (rigth)');
   console.log(c2 + '= c2 (left)');
   console.log(c3 + '= c3 (up)');
-  console.log(c4 + '= c4 (Bottom)');
-  console.log(min);
+  console.log(c4 + '= c4 (bottom)');
+  console.log(min +'= min');
+  console.log(min +'= min');
 
   drawField();
 
@@ -179,8 +195,8 @@ function fpp(i) {
 
 document.onkeypress = function (event) {
   if (event.keyCode == 13) {
-    goSearch();
     console.log('Hello World!');
+    goSearch();
   }
 }
 // document.onkeypress=function(event){
