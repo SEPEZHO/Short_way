fcf9fa1c11f247676b1c51233b14cac6f346df87
SEPEZHO <47150504+SEPEZHO@users.noreply.github.com>
2019-04-21 12:39:08 +0300

    A

--- script.js
+++ script.js
@@ -39,6 +39,10 @@ canvas.onclick = function (event) {
   } else {
     mas[y][x] = 4;
   }
+  if (event.altKey) {
+    console.log('asas');
+    mas[y][x] = 5;
+  }
   drawField();
 }
 
@@ -57,6 +61,9 @@ function drawField() {
       } else if (mas[i][j] == 4) {
         ctx.fillStyle = 'red';
         ctx.fillRect(j * 10, i * 10, 9.9, 9.9);
+      } else if (mas[i][j] == 5) {
+        mas[i][j] = 0;
+        ctx.clearRect(j * 10, i * 10, 9.9, 9.9);
       }
     }
   }
@@ -78,6 +85,7 @@ function goSearch() {
       if (mas[i][j] == 3) {
         var x3 = xxx3;
         var y3 = yyy3;
+        mas[i][j] = 1;
         ii = i;
         jj = j;
       }
@@ -90,91 +98,87 @@ function goSearch() {
       var aa = xx - x3;
       var bb = yy - y3;
       c1 = Math.sqrt(aa * aa + bb * bb);
-      // console.log(mas[i-1][j]+' =rigth');
 
       var xx1 = xxx2 + 1;
       var yy1 = yyy2;
       var aa1 = xx1 - x3;
       var bb1 = yy1 - y3;
       c2 = Math.sqrt(aa1 * aa1 + bb1 * bb1);
-      // console.log(mas[i+1][j]+' =left');
 
       var xx2 = xxx2;
       var yy2 = yyy2 + 1;
       var aa2 = xx2 - x3;
       var bb2 = yy2 - y3;
       c3 = Math.sqrt(aa2 * aa2 + bb2 * bb2);
-      // console.log(mas[i][j+1]+' =up');
 
       var xx3 = xxx2;
       var yy3 = yyy2 - 1;
       var aa3 = xx3 - x3;
       var bb3 = yy3 - y3;
       c4 = Math.sqrt(aa3 * aa3 + bb3 * bb3);
-      // console.log(mas[i][j-1]+' =bottom');
-
     }
   }
-  if (mas[ii][jj+1] ==4 || mas[ii][jj+1] ==1 ) { // rigth
+  if (mas[ii][jj + 1] == 4 || mas[ii][jj + 1] == 1) { // rigth
     c1 = 100;
-    // console.log(mas[i-1][j]+' =rigth1');
   }
-  if (mas[ii][jj-1] ==4 || mas[ii][jj-1] ==1 ) { //left
+  if (mas[ii][jj - 1] == 4 || mas[ii][jj - 1] == 1) { //left
     c2 = 100;
-    // console.log(mas[i+1][j]+' =left1');
   }
-  if (mas[ii-1][jj] ==4 || mas[ii-1][jj] ==1 ) { // up
+  if (mas[ii - 1][jj] == 4 || mas[ii - 1][jj] == 1) { // up
     c3 = 100;
-    // console.log(mas[i][j+1]+' =up1');
   }
-  if (mas[ii+1][jj] ==4 || mas[ii+1][jj] ==1 ) { // bottom
+  if (mas[ii + 1][jj] == 4 || mas[ii + 1][jj] == 1) { // bottom
     c4 = 100;
-    // console.log(mas[i][j-1]+' =bottom1');
   }
-  // var min;
-  // for(var minA = 0; minA <= 4;minA++){
-  //   if(('c'+minA) != undefined){
-  //     if(min<='c'+minA){
-  //       min = 'c'+minA;
-  //       console.log('c'+minA,min);
-  //     }
-  //   }
-  // }
-  if(c1 == c3||c2==c4 )
+
+  if (c1 == c2 && (c3 == 100 || c4 == 100)) {
+    var arr = [c1, c2];
+    var ran = Math.floor(Math.random() * (2 - 1 + 2)) + 1;
+    ran = ran - 1;
+    if (arr[ran] == c1) {
+      c2 = c2 - 1;
+    } else {
+      c1 = c1 - 1;
+    }
+  }
+  if (c3 == c4 && (c1 == 100 || c2 == 100)) {
+    var arr = [c3, c4];
+    var ran = Math.floor(Math.random() * (2 - 1 + 2)) + 1;
+    ran = ran - 1;
+    if (arr[ran] == c3) {
+      c3 = c3 - 1;
+    } else {
+      c4 = c4 - 1;
+    }
+  }
   var min = Math.min(c, c1, c2, c3, c4);
-   console.log(c1, c2, c3, c4);
 
-   console.log(min +'= min1');
-
-  if (min == c1 ) {
+  if (min == c1) {
     mas[ii][jj + 1] = 3;
     xxx3++;
-    console.log('c1');
-  } else if (min == c2  ) {
+    console.log('rigth (c1)');
+  } else if (min == c2) {
     mas[ii][jj - 1] = 3;
     xxx3--;
-    console.log('c2');
-
-  } else if (min == c3 ) {
+    console.log('left (c2)');
+  } else if (min == c3) {
     mas[ii - 1][jj] = 3;
-
-    console.log('c3');
     yyy3--;
-  } else if (min == c4 ) {
+    console.log('up (c3)');
+  } else if (min == c4) {
     mas[ii + 1][jj] = 3;
-    console.log('c4');
     yyy3++;
+    console.log('bottom (c4)');
   }
-  mas[ii][jj] = 1;
 
-  console.log(c1 + '= c1 (rigth)');
-  console.log(c2 + '= c2 (left)');
-  console.log(c3 + '= c3 (up)');
-  console.log(c4 + '= c4 (bottom)');
-  console.log(min +'= min');
-  console.log(min +'= min');
+  console.log('rigth (c1) =  ' + c1);
+  console.log('left (c2) =   ' + c2);
+  console.log('up (c3) =     ' + c3);
+  console.log('bottom (c4) = ' + c4);
+  console.log('min =         ' + min);
 
   drawField();
+  console.log('<=============================>');
 
   if (c !== c) {
     console.log('Цикл закончен')
@@ -183,30 +187,9 @@ function goSearch() {
   }
 }
 
-function fpm(i) {
-  if (i == 0) return width;
-  else return i;
-}
-
-function fpp(i) {
-  if (i == width - 1) return -1;
-  else return i;
-}
-
 document.onkeypress = function (event) {
   if (event.keyCode == 13) {
     console.log('Hello World!');
     goSearch();
   }
 }
-// document.onkeypress=function(event){
-//   if(event.shiftKey){
-//     for (var i = 0; i < width; i++) {
-//       for (var j = 0; j < height; j++) {
-//         if (mas[i-1][j] == 3){
-//           console.log(mas, mas[i-1][j]);
-//         }
-//       }
-//     }
-//   }
-// }
