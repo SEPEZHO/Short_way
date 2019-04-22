340b55db130f5e714f5b126b57827b7eb5c64b44
SEPEZHO <47150504+SEPEZHO@users.noreply.github.com>
2019-04-22 15:38:12 +0300

    a

--- script.js
+++ script.js
@@ -73,51 +73,45 @@ function goSearch() {
   var c1;
   var c2;
   var c3;
-  var c4
+  var c4;
   outer: for (var i = 1; i < width / 10; i++) {
     for (var j = 1; j < height / 10; j++) {
 
-      if (mas[i][j] == 2) {
-        var x2 = xxx2;
-        var y2 = yyy2;
-      }
-
       if (mas[i][j] == 3) {
-        var x3 = xxx3;
-        var y3 = yyy3;
         mas[i][j] = 1;
         ii = i;
         jj = j;
       }
-      var a = x2 - x3;
-      var b = y2 - y3;
-      var c = Math.sqrt(a * a + b * b);
-
-      var xx = xxx2 - 1;
-      var yy = yyy2;
-      var aa = xx - x3;
-      var bb = yy - y3;
-      c1 = Math.sqrt(aa * aa + bb * bb);
-
-      var xx1 = xxx2 + 1;
-      var yy1 = yyy2;
-      var aa1 = xx1 - x3;
-      var bb1 = yy1 - y3;
-      c2 = Math.sqrt(aa1 * aa1 + bb1 * bb1);
-
-      var xx2 = xxx2;
-      var yy2 = yyy2 + 1;
-      var aa2 = xx2 - x3;
-      var bb2 = yy2 - y3;
-      c3 = Math.sqrt(aa2 * aa2 + bb2 * bb2);
-
-      var xx3 = xxx2;
-      var yy3 = yyy2 - 1;
-      var aa3 = xx3 - x3;
-      var bb3 = yy3 - y3;
-      c4 = Math.sqrt(aa3 * aa3 + bb3 * bb3);
     }
   }
+
+  var a = xxx2 - xxx3;
+  var b = yyy2 - yyy3;
+  var c = Math.sqrt(a * a + b * b);
+
+  var xx = xxx2 - 1;
+  var aa = xx - xxx3;
+  var bb = yyy2 - yyy3;
+  c1 = Math.sqrt(aa * aa + bb * bb);
+
+  var xx1 = xxx2 + 1;
+  var aa1 = xx1 - xxx3;
+  var bb1 = yyy2 - yyy3;
+  c2 = Math.sqrt(aa1 * aa1 + bb1 * bb1);
+
+
+  var yy2 = yyy2 + 1;
+  var aa2 = xxx2 - xxx3;
+  var bb2 = yy2 - yyy3;
+  c3 = Math.sqrt(aa2 * aa2 + bb2 * bb2);
+
+
+  var yy3 = yyy2 - 1;
+  var aa3 = xxx2 - xxx3;
+  var bb3 = yy3 - yyy3;
+  c4 = Math.sqrt(aa3 * aa3 + bb3 * bb3);
+
+
   if (mas[ii][jj + 1] == 4 || mas[ii][jj + 1] == 1 || mas[ii][jj + 1] !== mas[ii][jj + 1]) { // rigth
     c1 = 100;
   }
@@ -178,14 +172,14 @@ function goSearch() {
   console.log('bottom (c4) = ' + c4);
   console.log('min =         ' + min);
 
-  drawField();
-  console.log('<=============================>');
-
-  if (c !== c) {
+  if (min == 0) {
     console.log('Цикл закончен')
   } else {
     timer = setTimeout(goSearch, 50);
   }
+  drawField();
+  console.log('<=============================>');
+
 }
 
 document.onkeypress = function (event) {
